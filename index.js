ed080ad895f41e722245f26a95a29ccafedd52fb
Josh <bluemazaro@yahoo.com>
2015-02-03 05:09:57 -0800

    bugfix commit

--- index.js
+++ index.js
@@ -3,3 +3,5 @@
 var hello = require('./lib/hello.js');
 
 console.log(hello.greet("Joshua"));
+console.log(hello.greet("Toasty"));
+console.log(hello.greet("life ov code"));
--- test/hello_test.js
+++ test/hello_test.js
@@ -4,7 +4,7 @@ var expect = require("chai").expect;
 var hello = require('../lib/hello.js');
 
 describe('hello function', function() { 
-	it('should return hello world', function () {
+	it('should greet a person by name', function () {
 		expect(hello.greet()).to.not.eql('goodbye');
 		expect(hello.greet("Joshua")).to.eql("hello Joshua");
 	});
